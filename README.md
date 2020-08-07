3e7119551f36e5a752960e808e08729fdb511765
Abbas Dawood <abbas@bizongo.com>
2020-08-07 14:19:16 +0530

    Finishing touches

---
+++ README.md
@@ -0,0 +1,59 @@
+## Mailadin
+---
+A utility that allows to generate rich HTML based mail templates for transactional and marketing needs, based on preexisting partials. By using this generator, you would no longer need to handcraft your way using raw HTML, neither would you have to spend time designing inconsistent templates using a WYSIWYG editor. Best used for teams that need consistency in mail templates, but are finding it hard to achieve because of rapid distributed development.
+
+### Setup
+- Install the package by using `npm i -g mailadin`
+- Open shell of choice, `mkdir test && cd _`
+- Write `mailadin`
+- Open the output file generated in the browser, copy it into where you needed the template and modify the content
+
+### Generating templates
+To generate template, `cd` into any folder where you want to output the files and run `mailadin`
+
+### Components
+There are 6 components available out of the box, they are:
+
+| Component | Variant                 |  Description |   |
+|-----------|-------------------------|--------------|---|
+| Text      | Block | Block of text | |
+| Text      | With Left Image | Block of text with an image floated to the left | |
+| Text      | With Top Image | Block of text with an image floated to the top of the text | |
+| Text      | Warning | Block of text highlighted specifically for communication of pre-formatted text and error / warning messages | |
+| Table      | Horizontal | Table with a header row and body | |
+| Table      | Vertical | Table with a single column with header row and body | |
+
+#### Options
+- `template` - Use this option to switch between marketing and transactional header and footer style, by default the generator uses the transactional template. Allowed values are `transactional` (default) & `marketing`
+- `body` - Use this option to add content between the generated header and footer
+- `path` - Where to output the files to, defaults to cwd
+
+
+| Key | Styles         | Supported in  |
+|-----|----------------|---------------|
+| text |`text:block` (default), `text:image_left, text:warning, text:image_top`| `transactional` & `marketing` |
+| table | `table:vertical, table:horizontal` | `transactional` & `marketing` |
+| button | `button:center` (default), `button:left, button:right` | `transactional` & `marketing` |
+
+
+#### CLI Usage Samples
+- To generate a mail template with a block text and warning text block, you would write: `mailadin --template=transactional --body=text:block,text:warning`
+
+```
+Usage: -t <template type>
+
+Options:
+  --help          Show help                                            [boolean]
+  --version       Show version number                                  [boolean]
+  -t, --template  Which template to use, one of marketing or transactional
+                                                             [string] [required]
+  -b, --body      Comma separated values from this list: text(:block|:warning|:i
+                  mage_left|:image_top),button(:left|:center|:right),table
+                  (:vertical|:horizontal).                              [string]
+  -p, --path      Absolute path where the files have to be generated, default is
+                  current working directory                             [string]
+
+```
+
+### Contributing
+To contribute to this project, please contact the author [@abbasdawood](https://github.com/abbasdawood)
---
+++ bin/index.js
@@ -0,0 +1,131 @@
+#! /usr/bin/env node
+const _ = require('lodash');
+let fs = require('fs');
+const yargs = require("yargs");
+const chalk = require('chalk');
+
+let promises = []
+
+const boxenOptions = {
+    backgroundColor: "black"
+};
+
+let sections = {
+    transactional: {
+        header: 'header_txnal',
+        footer: 'footer_txnal'
+    },
+    marketing: {
+        header: 'header_marketing',
+        footer: 'footer_marketing'
+    },
+    table: {
+        vertical: 'table_v',
+        horizontal: 'table_h'
+    },
+    text: {
+        block: 'text_block',
+        warning: 'text_warning',
+        image_left: 'text_image_left',
+        image_top: 'text_image_top'
+    },
+    button: {
+        center: 'button_block_center',
+        left: 'button_block_left',
+        right: 'button_block_right'
+    },
+    head: 'head'
+}
+
+let defaults = {
+    template: 'transactional',
+    body: 'text,table:horizontal,button'
+}
+
+const options = yargs
+    .usage('Usage: -t <template type>')
+    .option('t', { alias: 'template', describe: "Which template to use, one of marketing or transactional", type: 'string', demandOption: true })
+    .option('b', { alias: 'body', describe: "Comma separated values from this list: text(:block|:warning|:image_left|:image_top),button(:left|:center|:right),table (:vertical|:horizontal).", type: 'string', demandOption: false })
+    .option('p', { alias: 'path', describe: "Absolute path where the files have to be generated, default is current working directory", type: 'string', demandOption: false })
+    .argv;
+
+/**
+ * Function to build the final template based on inputs given by devs
+ */
+function main() {
+    let args = process.argv.slice(1)
+
+    console.log(chalk.green.bold('💡Rubbing the lamp..'));
+
+    let customisation = {}
+
+    customisation.template = options.template || defaults.template
+    customisation.body = options.body || defaults.body
+    customisation.cwd = options.path || '.'
+
+    createMarkup(customisation)
+}
+
+function createMarkup(options) {
+
+    let header = sections[options.template].header
+    let footer = sections[options.template].footer
+
+    console.log(chalk.white.bold('📝 Writing head..'));
+
+    promises.push(fs.readFileAsync(options.cwd, sections.head))
+
+    console.log(chalk.white.bold(`📝 Building ${options.template} template..`));
+
+    promises.push(fs.readFileAsync(options.cwd, header))
+
+    console.log(chalk.white.bold('🍼 Adding body...'));
+
+    _.forEach(options.body.split(','), partial => {
+        let s = partial.split(':')
+        if (s.length > 1) {
+            promises.push(fs.readFileAsync(options.cwd, sections[s[0]][s[1]]))
+        } else {
+            let keys = _.keys(sections[s[0]])
+            promises.push(fs.readFileAsync(options.cwd, sections[s[0]][keys[0]]))
+        }
+    })
+
+
+    promises.push(fs.readFileAsync(options.cwd, footer))
+
+    Promise.all(promises).then(data => {
+        console.log(chalk.white.bold('✅ Finishing up..'));
+        writeTemplate(options.cwd, data.join('\n'), 'email_output', 'html')
+    })
+}
+
+function writeTemplate(wd, content, filename, format) {
+    let file = wd + `/${filename}.${format}`
+    fs.writeFile(file, content, 'utf-8', (err) => {
+        if (err) {
+            console.error(err);
+        }
+        console.log('📧 Output generated at: ' + chalk.green(file));
+    });
+}
+
+fs.readFileAsync = (cwd, partialName) => {
+    return new Promise((resolve, reject) => {
+        // TODO: allow override of partials
+        let path = __dirname + `/partials/${partialName}.txt`
+        fs.readFile(path, { encoding: 'utf-8' }, (err, data) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(data)
+            }
+        });
+    });
+}
+
+module.exports = {
+    main: main
+}
+
+main();
